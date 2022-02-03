d6aea0398b30f9629cea9348e9dd41aaea36c2d4
David W de Jesus <davidwj15@gmail.com>
2022-02-03 11:19:52 -0300

    adicionado api - filtros de filmes por genero

--- src/components/list/list.jsx
+++ src/components/list/list.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./list.css";
 
 export default function List() {
+  const [movies, setMovies] = useState([]);
+  const apiKey = "8cfb3f7b5d20b29a8bb4602b47a77292";
+  const page = 1;
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-BR&page=${page}`
+      )
+      .then((results) => {
+        const result = results.data.results;
+        setMovies(result);
+      });
+  }, []);
   return (
     <div className="list">
       <div className="containerList">
-        filme
+        {movies.map((movie) => (
+          <div className="containerFilme" key={movie.id}>
+            <img
+              className="posterFilme"
+              alt="Movie"
+              src={`https://image.tmdb.org/t/p/w500` + movie.poster_path}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/top/top.jsx
+++ src/components/top/top.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./top.css";
 
 export default function Top() {
+  const [genres, setGenres] = useState([]);
+  const apiKey = "8cfb3f7b5d20b29a8bb4602b47a77292";
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=pt-BR`
+      )
+      .then((results) => {
+        const result = results.data.genres;
+        setGenres(result);
+      });
+  }, []);
   return (
     <div className="top">
       <div className="containerTop">
@@ -9,12 +23,12 @@ export default function Top() {
           Milhões de filmes, séries e pessoas para descobrir. Explore já.
         </span>
         <div className="containerFiltros">
-            <span>Filtre por:</span>
-            <div className="botaoFiltros">
-                <button className="filtros">
-                    Teste
-                </button>
-            </div>
+          <span>Filtre por:</span>
+          <div className="botaoFiltros" >
+            {genres.map((genre) => (
+              <button className="filtros" key={genre.id}>{genre.name}</button>
+            ))}
+          </div>
         </div>
       </div>
     </div>
