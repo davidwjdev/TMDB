7ec4ac3fdee6c59a36eb96b6234a945fbaaa7775
David W de Jesus <davidwj15@gmail.com>
2022-02-04 19:03:13 -0300

    adicionado paginação da lista de filmes

--- src/components/list/list.jsx
+++ src/components/list/list.jsx
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from "react";
+import Pagination from "@mui/material/Pagination";
+
 import axios from "axios";
+import moment from "moment";
+
+import "moment/locale/pt-br";
 import "./list.css";
 
 export default function List() {
   const [movies, setMovies] = useState([]);
   const apiKey = "8cfb3f7b5d20b29a8bb4602b47a77292";
-  const page = 1;
+  const [page, setPage] = useState(3);
+  const maxPages = 100;
+
 
   useEffect(() => {
     axios
@@ -16,7 +23,13 @@ export default function List() {
         const result = results.data.results;
         setMovies(result);
       });
-  }, []);
+  }, [page]);
+ 
+  const handleChange = (event, value) => {
+    setPage(value);
+    console.log(value);
+  };
+
   return (
     <div className="list">
       <div className="containerList">
@@ -27,8 +40,15 @@ export default function List() {
               alt="Movie"
               src={`https://image.tmdb.org/t/p/w500` + movie.poster_path}
             />
+            <span className="nomeFilme">{movie.title}</span>
+            <span className="dataLancamentoFilme">
+              {moment(movie.release_date).locale("pt").format("DD MMM YYYY")}
+            </span>
           </div>
         ))}
+        <div className="paginacao">
+          <Pagination count={maxPages} page={page} onChange={handleChange} />
+        </div>
       </div>
     </div>
   );
